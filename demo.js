43ec4b2f873db8ff78a7da80b5be560b1a85576c
Salvatore Laisa <moebiusmania@gmail.com>
2018-10-15 16:02:43 +0200

    add demo action/code

---
+++ demo.js
@@ -0,0 +1,21 @@
+
+const title = 'Google Assistant deployed page';
+const time = new Date().toUTCString();
+
+const buildHtml = (req) => /*html*/`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${title}</title>
+  </head>
+  <body>
+    <h1>${title}</h1>
+    <p>File generated on <strong>${time}</strong></p>
+  </body>
+  </html>
+`;
+
+console.log(buildHtml());
